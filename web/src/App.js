9f447e838cc858c706db50a3a97219c5a07aea60
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2022-03-13 18:28:22 +0700

    Test add axios in web

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import axios from 'axios';
+
+const increase = async () => {
+  let result = await axios.get('http://localhost:3000/increase')
+  number++;
+}
+
+const refresh = async () => {
+  let result = await axios.get('http://localhost:3000/refresh')
+  number = result.data;
+}
 
 var number = 0;
 function App() {
   return (
     <div className="App">
       <p1>{number}</p1>
-      <button>Click Me to increase</button>
-      <button>Click Me to refresh</button>
+      <button onClick={increase}>Click Me</button>
+      <button onClick={refresh}>Click Me</button>
     </div>
   );
 }
