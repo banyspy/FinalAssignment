fc707a18faeb91f89d197558726b026618c954f5
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2022-03-19 17:16:18 +0700

    Add request in react to fetch data when refresh from server

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import cat_c from 'img/popcat_mouse_close.img'
 import cat_o from 'img/popcat_mouse_open.img'
 
+
+
 const increase = async () => {
   let result = await axios.get('http://localhost:3000/increase')
   number++;
@@ -15,6 +17,14 @@ const refresh = async () => {
 
 var number = 0;
 function App() {
+  const [refreshdata, setRefreshdata] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/refresh")
+      .then((res) => res.json())
+      .then((refreshdata) => setRefreshdata(refreshdata.data));
+  }, []);
+
   return (
     <div className="App">
       <p1>{number}</p1>
