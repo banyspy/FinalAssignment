b1d3e60cbd49b508a70b6840074ddc7504bee617
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2022-03-29 18:18:21 +0700

    Add route to main file

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var increaseRouter = require('./routes/increase');
+var refreshRouter = require('./routes/refresh');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/increase', increaseRouter);
+app.use('/refresh', refreshRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
