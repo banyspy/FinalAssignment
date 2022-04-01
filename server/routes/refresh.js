1b3b16287759487e9586610fe03a28538ef25232
mingmaomak <61099491+mingmaomak@users.noreply.github.com>
2022-04-01 23:05:24 +0700

    Rename sound files and some stuff

--- server/routes/refresh.js
+++ server/routes/refresh.js
@@ -14,7 +14,7 @@ router.get('/', function(req, res, next) {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Content-Type', 'application/json');
-    res.json({"data": value.Total});
+    res.json({"data": value.Total}); //please don't read the following line I was dumb res.json() is for sending a JSON response. https://expressjs.com/en/guide/routing.html
   }); //The json() method of the Response interface takes a Response stream and reads it to completion. It returns a promise which resolves with the result of parsing the body text as JSON.
 
 module.exports = router;
