bd24130a5d0743afce56ce2315ad1dc74cd558ea
mingmaomak <61099491+mingmaomak@users.noreply.github.com>
2022-04-01 09:56:14 +0700

    I'm tired

--- web/src/App.js
+++ web/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import axios from 'axios';
 import cat_c from './img/popcat_mouse_close.png'
 import cat_o from './img/popcat_mouse_open.png'
-import BackgroundCat from './BackgroundCat.js';
-
+// import BackgroundCat from './BackgroundCat.js';
+// import BackgroundCat2 from './BackgroundCat2.js';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 /*
 const increase = async () => {
@@ -18,12 +19,28 @@ const refresh = async () => {
   number = result.data;
 }
 */
+const imagePath = {
+  close: cat_c,
+  open: cat_o
+};
 
 var number = 0;
 class App extends Component{
   constructor(props) {
     super(props);
     this.state = { apiResponse: "" };
+  }
+
+  getImageName(){ 
+    return(this.state.isOff ? 'close' : 'open' )
+  }
+
+  handlePressIn() {
+    this.setState({isOff:false});
+  }
+
+  handlePressOut() {
+    this.setState({isOff:true});
   }
 
   callIncrease() {
@@ -42,17 +59,41 @@ class App extends Component{
   }
 
   render() {
+    const imageName = this.getImageName();
     return (
         <div className="App">
-          <BackgroundCat/>
+          <Pressable onClick={this.callIncrease} onPressIn={this.handlePressIn} onPressOut={this.handlePressOut}>
+          <img is="cat" id="cat" onClick={this.handleClick} src={imagePath[imageName]}  alt="cat"></img>
+          </Pressable>
           <h is="h">{number}</h>
-          <br/>
-          <button onClick={this.callIncrease}>increase</button>
           <br/>
           <button onClick={this.callRefresh}>refresh</button>
         </div>
     );
   };
 }
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    padding: 10,
+    borderWidth: 3,
+    borderColor: "#111",
+  },
+  text: {
+    backgroundColor: "transparent",
+    color: "#111",
+  },
+  bgFill: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+  },
+});
 
 export default App;
