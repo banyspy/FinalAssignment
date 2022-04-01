5c18fcc227f6083989bdf3deab53df6d3c52b35b
mingmaomak <61099491+mingmaomak@users.noreply.github.com>
2022-04-01 13:20:33 +0700

    Cat can now open mouth, although be careful as I might screwed up the packaging system, please blame NPM

--- web/src/App.js
+++ web/src/App.js
@@ -1,3 +1,4 @@
+
 import React, {Component} from 'react';
 import './App.css';
 import axios from 'axios';
@@ -5,7 +6,7 @@ import cat_c from './img/popcat_mouse_close.png'
 import cat_o from './img/popcat_mouse_open.png'
 // import BackgroundCat from './BackgroundCat.js';
 // import BackgroundCat2 from './BackgroundCat2.js';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, TouchableOpacity, Image  } from 'react-native';
 
 /*
 const increase = async () => {
@@ -28,7 +29,10 @@ var number = 0;
 class App extends Component{
   constructor(props) {
     super(props);
-    this.state = { apiResponse: "" };
+    this.state = {
+      apiResponse: "" , 
+      isOff: true
+    };
   }
 
   getImageName(){ 
@@ -60,11 +64,34 @@ class App extends Component{
 
   render() {
     const imageName = this.getImageName();
+    // var icon_cat = this.state.isOff ?
     return (
         <div className="App">
-          <Pressable onClick={this.callIncrease} onPressIn={this.handlePressIn} onPressOut={this.handlePressOut}>
-          <img is="cat" id="cat" onClick={this.handleClick} src={imagePath[imageName]}  alt="cat"></img>
+          {/* <Image source={icon_cat} style={{width: 55, height: 55}} />
+          <View style={styles.container}>
+            <TouchableOpacity style={styles.button} onPress={this.callIncrease} onPressIn={this.handlePressIn} onPressOut={this.handlePressOut}>
+              <Text>Pop</Text>
+            </TouchableOpacity>
+          </View> */}
+          <Pressable
+            onPress={this.callIncrease}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                  ? 'rgb(210, 230, 255)'
+                  : 'white'
+              },
+              styles.wrapperCustom
+            ]}>
+            {({ pressed }) => (
+              <Text style={styles.text}>
+              <Image is="cat" id="cat" source=  {pressed ?  require('./img/popcat_mouse_open.png') : require('./img/popcat_mouse_close.png') } style={{width: 400, height: 400}} alt="cat" />
+              </Text>
+            )}
           </Pressable>
+          {/* <Pressable onClick={this.callIncrease} onPressIn={this.handlePressIn} onPressOut={this.handlePressOut}>
+            <Image is="cat" id="cat" source={icon_cat} style={{width: 400, height: 400}} alt="cat" />
+          </Pressable> */}
           <h is="h">{number}</h>
           <br/>
           <button onClick={this.callRefresh}>refresh</button>
@@ -72,28 +99,43 @@ class App extends Component{
     );
   };
 }
-
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
     justifyContent: "center",
   },
-  button: {
-    padding: 10,
-    borderWidth: 3,
-    borderColor: "#111",
-  },
   text: {
-    backgroundColor: "transparent",
-    color: "#111",
+    fontSize: 16
   },
-  bgFill: {
-    position: "absolute",
-    top: 0,
-    left: 0,
+  wrapperCustom: {
+    borderRadius: 8,
+    padding: 6
   },
+  logBox: {
+    padding: 20,
+    margin: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#f0f0f0',
+    backgroundColor: '#f9f9f9'
+  }
 });
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   button: {
+//     backgroundColor: '#859a9b',
+//     borderRadius: 20, //the radius on the corner
+//     padding: 100,
+//     marginBottom: 20, //padding below the image
+//     shadowColor: '#303838',
+//     shadowOffset: { width: 0, height: 5 },
+//     shadowRadius: 10,
+//     shadowOpacity: 0.35,
+//   },
+// });
 
 export default App;
