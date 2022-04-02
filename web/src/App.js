8306e8aeded391ccce28a407c0a577b09fdf5e50
mingmaomak <61099491+mingmaomak@users.noreply.github.com>
2022-04-02 16:21:36 +0700

    Deleted more unused code, added some code for background image but it doesn't fucking work

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,14 @@
-import React, {Component,useState,useEffect}from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import axios from 'axios';
 import Top from './Top.js';
-import { Pressable, StyleSheet, Text, View, TouchableOpacity, Image  } from 'react-native';
+import {ImageBackground, Pressable, StyleSheet, Text, View, TouchableOpacity, Image  } from 'react-native';
+
 class App extends Component{
   constructor(props) {
     super(props);
-    this.state = { 
-      numberUser:0,
-      numberTotal: 0,
-      isOff: true};
+    this.state = { input:[],numberUser:0,numberTotal: 0 ,isOff: true};
     this.callRefresh = this.callRefresh.bind(this);
-  }
-
-  getImageName(){ 
-    return(this.state.isOff ? 'close' : 'open' )
   }
 
   callIncrease() {
@@ -38,59 +32,40 @@ class App extends Component{
   }
 
   render(){
-    const imageName = this.getImageName();
-      return(<div className="App">
-        <Top/>
-        <Pressable
-            //onPressIn={new Audio("./sound/sound_open.mp3").play()}
-            //onPressOut={new Audio("./sound/sound_close.mp3").play()}
-            onPress={() => {this.callIncrease();this.callRefresh();this.setState({numberUser: this.state.numberUser + 1})} }
-            style={({ pressed }) => [
-              {
-                backgroundColor: pressed
-                  ? 'rgb(210, 230, 255)'
-                  : 'white'
-              },
-              styles.wrapperCustom
-            ]}>
-            {({ pressed }) => (
-              <Text style={styles.text}>
-              <Image is="cat" id="cat" source=  {pressed ?  require('./img/popcat_mouse_open.png') : require('./img/popcat_mouse_close.png') } style={{width: 400, height: 400}} alt="cat" />
-              </Text>
-            )}
-          </Pressable>
-        <br/>
-        <br/>
-        <h is="h">Current for this user: {this.state.numberUser}</h>
-        <br/>
-        <br/>
-        <h is="h">Total amount: {this.state.numberTotal}</h>
-        <br/>
-        <br/>
-        <button onClick={this.callRefresh}>refresh</button>
-        <br/>
-        <br/>
-      </div>)
-    }
+    return(<div className="App">
+      <View>
+        <ImageBackground source={{uri:"./img/Bliss.jpg"}} resizeMode="contain" style={styles.ImageBackground}> {/*currently background image doesn't work whatever I did */}
+          <Top/>
+          <Pressable
+            onPress={() => {this.callIncrease();this.callRefresh();this.setState({numberUser: this.state.numberUser + 1})} }>
+              {({ pressed }) => (
+                <Text>
+                <Image is="cat" id="cat" source={pressed ?  require('./img/popcat_mouse_open.png') : require('./img/popcat_mouse_close.png') } style={{width: 400, height: 400}} alt="cat" />
+                </Text>
+              )}
+            </Pressable>
+          <br/>
+          <br/>
+          <h is="h">Current for this user: {this.state.numberUser}</h>
+          <br/>
+          <br/>
+          <h is="h">Total amount: {this.state.numberTotal}</h>
+          <br/>
+          <br/>
+          <button onClick={this.callRefresh}>refresh</button>
+        </ImageBackground>
+      </View>
+    </div>)
+  }
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 16
-  },
   wrapperCustom: {
     borderRadius: 8,
     padding: 6
   },
-  logBox: {
-    padding: 20,
-    margin: 10,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderColor: '#f0f0f0',
-    backgroundColor: '#f9f9f9'
-  }
+  ImageBackground: {
+    flex: 1,
+    justifyContent: "center",
+  },
 });
 export default App;
